604843f27700c59719465936bd33a9ac5d40f2aa
VincenzoBorriello89 <vincborr89@gmail.com>
2022-05-31 16:44:34 +0200

    rotte stabilite e db selez

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Comic;
 
 class ComicController extends Controller
 {
@@ -13,7 +14,11 @@ class ComicController extends Controller
      */
     public function index()
     {
-        //
+
+        $comics = Comic::All();
+        // dd($comics);
+
+        return view('welcome', compact('comics'));
     }
 
     /**
