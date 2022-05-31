13a0b2a2d9e97d41f0b0303a2d2d3389e0ef815b
VincenzoBorriello89 <vincborr89@gmail.com>
2022-05-31 15:56:49 +0200

    migrate tab comics su myadmin

--- database/migrations/2022_05_31_134004_create_comics_table.php
+++ database/migrations/2022_05_31_134004_create_comics_table.php
@@ -15,6 +15,13 @@ class CreateComicsTable extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->text('thumb');
+            $table->float('price', 4, 2);
+            $table->string('series');
+            $table->date('sale_date');
+            $table->string('type');
             $table->timestamps();
         });
     }
